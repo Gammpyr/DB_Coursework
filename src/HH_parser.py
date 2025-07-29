d845cd1c4c11df9b5a2be4ecd0556c67b52dbe6f
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-07-29 21:52:52 +0300

    update parser

--- src/HH_parser.py
+++ src/HH_parser.py
@@ -9,18 +9,15 @@ def get_employee_info():
     result = []
     data = read_file()
 
-    # for emp in data:
-    #     response = requests.get(f'https://api.hh.ru/employers/{emp}')
-    #     response.raise_for_status()
-    #     result.append(response.json())
+    for emp in data:
+        response = requests.get(f'https://api.hh.ru/employers/{emp}')
+        response.raise_for_status()
+        result.append(response.json())
 
-
-    response = requests.get(f'https://api.hh.ru/employers/{data[0]}')
-    response.raise_for_status()
-    result.append(response.json())
     return result
 
-data["vacancies_url"] # url для поиска вакансий
+def get_vacancies(emp_info):
+    emp_info["vacancies_url"]  # url для поиска вакансий
 
 if __name__ == '__main__':
     save_to_file(get_employee_info())
