d863da23d88f289e2d475d80e54553d8628aa25d
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-08-01 22:46:59 +0300

    add HH parsers

--- src/HH_parser.py
+++ src/HH_parser.py
@@ -1,23 +1,59 @@
-from pprint import pprint
-
 import requests
 
-from src.file_workers import read_file, save_to_file
 
-
-def get_employee_info():
+def get_employers_info(data: list) -> list:
+    """
+    Принимает словарь с работодателями и возвращает информацию о них
+    """
     result = []
-    data = read_file()
 
-    for emp in data:
-        response = requests.get(f'https://api.hh.ru/employers/{emp}')
+    for key, value in data[0].items():
+        response = requests.get(f'https://api.hh.ru/employers/{value}')
         response.raise_for_status()
-        result.append(response.json())
+        response = response.json()
+        result.append({
+            'id': response['id'],
+            'name': response['name'],
+            'alternate_url': response['alternate_url'],
+            'vacancies_url': response['vacancies_url'],
+            'open_vacancies': response['open_vacancies'],
+        })
 
     return result
 
-def get_vacancies(emp_info):
-    emp_info["vacancies_url"]  # url для поиска вакансий
 
-if __name__ == '__main__':
-    save_to_file(get_employee_info())
+def get_vacancies(emps_info: list):
+    """Принимает список словарей с информацией о работодателях и возвращает список вакансий"""
+    result = []
+
+    for emp in emps_info:
+        params = {'per_page': 100, 'page': 0}
+        emp["vacancies"] = []
+        emp_vacancy_url = emp["vacancies_url"]
+        print(f'Получаем вакансии от: {emp['name']}')
+        while True:
+            response = requests.get(f'{emp_vacancy_url}', params)
+            response.raise_for_status()
+            response = response.json()
+
+            items = []
+            for item in response['items']:
+                items.append({
+                    "id": item["id"],
+                    "name": item["name"],
+                    "salary": item["salary"],
+                    "url": item["alternate_url"],
+                    "employer": item["employer"]["name"],
+                    "snippet": item["snippet"],
+                    "experience": item["experience"]["name"]}
+                )
+
+            emp["vacancies"].extend(items)
+
+            if params['page'] == response['pages'] - 1:
+                result.append({emp['name']: emp})
+                break
+            else:
+                params['page'] += 1
+
+    return result
