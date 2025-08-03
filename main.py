544d1f44d911abaeda310f2a6471ebae269e68d9
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-08-03 15:15:18 +0300

    update main.py

--- main.py
+++ main.py
@@ -4,47 +4,41 @@ import psycopg2
 
 from config import config
 from src.HH_parser import get_vacancies, get_employers_info
-from src.file_workers import read_file, save_to_file
+from src.database import DBworker, create_tables, fill_tables
+from src.dbmanager import DBManager
 
-data = read_file('employers')  # Читаем файл с ID работодателей
-
-# employers_info = get_employers_info([{"Компания 05.ру": 1150295}])
-employers_info = get_employers_info(data)  # Получаем информацию о работодателях
-
-get_vacancies(employers_info)  # Получаем список вакансий
+from src.file_workers import read_file
 
 
 
-# def main():
-#     script_file = 'fill_db.sql'
-#     json_file = 'suppliers.json'
-#     db_name = 'HH_employer'
-#
-#     params = config()
-#     conn = psycopg2.connect(**params)
-#
-#     create_database(params, db_name)
-#     print(f"БД {db_name} успешно создана")
-#
-#     params.update({'dbname': db_name})
-#     try:
-#         with psycopg2.connect(**params) as conn:
-#             with conn.cursor() as cur:
-#                 execute_sql_script(cur, script_file)
-#                 print(f"БД {db_name} успешно заполнена")
-#
-#                 create_suppliers_table(cur)
-#                 print("Таблица suppliers успешно создана")
-#
-#                 suppliers = get_suppliers_data(json_file)
-#                 insert_suppliers_data(cur, suppliers)
-#                 print("Данные в suppliers успешно добавлены")
-#
-#                 add_foreign_keys(cur, json_file)
-#                 print(f"FOREIGN KEY успешно добавлены")
-#
-#     except(Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
+
+def main():
+    data = read_file('employers')  # Читаем файл с ID работодателей
+    employers_info = get_employers_info(data)  # Получаем информацию о работодателях указанных в файле
+    # employers_info = get_employers_info([{"HobbyWorld": 962612}])  # Получаем информацию о работодателях указанных в файле
+
+    db_name = 'hh_employer'
+    params = config()
+
+    db = DBworker(**params)
+    db.create_database(db_name) # создаём базу данных
+    create_tables(db_name, params) # создаём таблицы
+    fill_tables(db_name, params, employers_info) # заполняем таблицы данными
+
+    # hh_employer = DBManager(db_name, **params)
+
+
+
+    # try:
+    #     with psycopg2.connect(**params) as conn:
+    #         with conn.cursor() as cur:
+
+    # except(Exception, psycopg2.DatabaseError) as error:
+    #     print(error)
+    # finally:
+    #     if conn is not None:
+    #         conn.close()
+
+
+if __name__=='__main__':
+    main()
